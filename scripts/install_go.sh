1c54648da8f36d73cc591ae001ac58ab76f6ab08
shono09835 <51314469+shono09835@users.noreply.github.com>
2024-02-08 18:49:05 +0900

    Update install_go.sh

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 GO_VERSION="1.19"
+expected_sha="7e231ea5c68f4be7fea916d27814cc34b95e78c4664c3eb2411e8370f87558bd"
 
 DOWNLOAD_FOLDER=/tmp/Downloads
 mkdir -p ${DOWNLOAD_FOLDER}
@@ -17,7 +18,7 @@ if [ ! -f ${DOWNLOAD_FILE} ]; then
 
   #GO_MD5="4577d9ba083ac86de78012c04a2981be"
   #URL=https://buildpacks.cloudfoundry.org/dependencies/go/go${GO_VERSION}.linux-amd64-${GO_MD5:0:8}.tar.gz
-  URL="https://buildpacks.cloudfoundry.org/dependencies/go/go_${version}_linux_x64_cflinuxfs3_${expected_sha:0:8}.tgz"
+  URL="https://buildpacks.cloudfoundry.org/dependencies/go/go_${GO_VERSION}_linux_x64_cflinuxfs3_${expected_sha:0:8}.tgz"
   
   echo "-----> Download go ${GO_VERSION}"
   curl -s -L --retry 15 --retry-delay 2 $URL -o ${DOWNLOAD_FILE}
@@ -32,11 +33,11 @@ else
   echo "-----> go install package available in cache"
 fi
 
-if [ ! -f $GoInstallDir/go/bin/go ]; then
+if [ ! -f $GoInstallDir/bin/go ]; then
   tar xzf ${DOWNLOAD_FILE} -C $GoInstallDir
 fi
 
-if [ ! -f $GoInstallDir/go/bin/go ]; then
+if [ ! -f $GoInstallDir/bin/go ]; then
   echo "       **ERROR** Could not download go"
   exit 1
 fi
