55f97ce9851309cc8ca3b002b802e621a3c9e7e4
Syuuto Ono (小野 修都) <syuuto.ono.cw@nttdocomo.com>
2024-02-08 14:23:18 +0900

    update go

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -1,43 +1,56 @@
 #!/bin/bash
-set -euo pipefail
 
-GO_VERSION="1.9"
+set -e
+set -u
+set -o pipefail
 
-DOWNLOAD_FOLDER=${CACHE_DIR}/Downloads
-mkdir -p ${DOWNLOAD_FOLDER}
-DOWNLOAD_FILE=${DOWNLOAD_FOLDER}/go${GO_VERSION}.tar.gz
-
-export GoInstallDir="/tmp/go$GO_VERSION"
-mkdir -p $GoInstallDir
-
-# Download the archive if we do not have it cached
-if [ ! -f ${DOWNLOAD_FILE} ]; then
-  # Delete any cached go downloads, since those are now out of date
-  rm -rf ${DOWNLOAD_FOLDER}/go*.tar.gz
-
-  GO_MD5="4577d9ba083ac86de78012c04a2981be"
-  URL=https://buildpacks.cloudfoundry.org/dependencies/go/go${GO_VERSION}.linux-amd64-${GO_MD5:0:8}.tar.gz
-
-  echo "-----> Download go ${GO_VERSION}"
-  curl -s -L --retry 15 --retry-delay 2 $URL -o ${DOWNLOAD_FILE}
-
-  DOWNLOAD_MD5=$(md5sum ${DOWNLOAD_FILE} | cut -d ' ' -f 1)
-
-  if [[ $DOWNLOAD_MD5 != $GO_MD5 ]]; then
-    echo "       **ERROR** MD5 mismatch: got $DOWNLOAD_MD5 expected $GO_MD5"
+function main() {
+  if [[ "${CF_STACK:-}" != "cflinuxfs3" && "${CF_STACK:-}" != "cflinuxfs4" ]]; then
+    echo "       **ERROR** Unsupported stack"
+    echo "                 See https://docs.cloudfoundry.org/devguide/deploy-apps/stacks.html for more info"
     exit 1
   fi
-else
-  echo "-----> go install package available in cache"
-fi
 
-if [ ! -f $GoInstallDir/go/bin/go ]; then
-  tar xzf ${DOWNLOAD_FILE} -C $GoInstallDir
-fi
+  local version expected_sha dir
+  version="1.19"
+  expected_sha="7e231ea5c68f4be7fea916d27814cc34b95e78c4664c3eb2411e8370f87558bd"
+  dir="/tmp/go${version}"
 
-if [ ! -f $GoInstallDir/go/bin/go ]; then
-  echo "       **ERROR** Could not download go"
-  exit 1
-fi
+  mkdir -p "${dir}"
 
+  if [[ ! -f "${dir}/bin/go" ]]; then
+    local url
+    # TODO: use exact stack based dep, after go buildpack has cflinuxfs4 support
+    #url="https://buildpacks.cloudfoundry.org/dependencies/go/go_${version}_linux_x64_${CF_STACK}_${expected_sha:0:8}.tgz"
+    url="https://buildpacks.cloudfoundry.org/dependencies/go/go_${version}_linux_x64_cflinuxfs3_${expected_sha:0:8}.tgz"
 
+    echo "-----> Download go ${version}"
+    curl "${url}" \
+      --silent \
+      --location \
+      --retry 15 \
+      --retry-delay 2 \
+      --output "/tmp/go.tgz"
+
+    local sha
+    sha="$(shasum -a 256 /tmp/go.tgz | cut -d ' ' -f 1)"
+
+    if [[ "${sha}" != "${expected_sha}" ]]; then
+      echo "       **ERROR** SHA256 mismatch: got ${sha}, expected ${expected_sha}"
+      exit 1
+    fi
+
+    tar xzf "/tmp/go.tgz" -C "${dir}"
+    rm "/tmp/go.tgz"
+  fi
+
+  if [[ ! -f "${dir}/bin/go" ]]; then
+    echo "       **ERROR** Could not download go"
+    exit 1
+  fi
+
+  GoInstallDir="${dir}"
+  export GoInstallDir
+}
+
+main "${@:-}"
